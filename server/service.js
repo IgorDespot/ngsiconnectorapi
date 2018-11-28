cfcfd10d82b01fd42578118d62e20b20e4191e92
Igor <igordesspot@gmail.com>
2018-11-28 15:50:46 +0100

    Add routes and erro handle to service

--- server/service.js
+++ server/service.js
@@ -2,6 +2,22 @@
 
 var service = require("express")();
 
+var { entitiesApi } = require("../components/entities");
+
 module.exports = function() {
+
+    service.use("/", entitiesApi);
+
+    service.use((req, res, next) => {
+        const error = new Error("Requested route not found");
+        error.status = 404;
+        next(error);
+    });
+
+    service.use((error, req, res, next) => {
+        res.status(error.status || 500);
+        res.json(error.message);
+    });
+
     return service;
 };

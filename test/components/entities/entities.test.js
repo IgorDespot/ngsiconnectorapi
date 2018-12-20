4e89e443bc1e6b375568bb4bc278e54b8972bb96
Igor Despot <igordesspot@gmail.com>
2018-12-20 08:34:24 -0800

    Add test for entity creation route

--- test/components/entities/entities.test.js
+++ test/components/entities/entities.test.js
@@ -2,6 +2,7 @@
 
 const request = require("supertest");
 const service = require("../../../server/service")();
+const fs = require("fs");
 
 describe("The entities component", function() {
     describe("GET /v2/entities", function() {
@@ -61,9 +62,45 @@ describe("The entities component", function() {
         });
     });
     describe("GET /v2/entities/:entityId", function() {
-        it("should return HTTP 200 when all preconditions are met", (done) => {
+        it("should return HTTP 404 when all preconditions are met", (done) => {
             request(service)
             .get("/v2/entities/badId")
+            .set("Fiware-Service", "test")
+            .set("Fiware-ServicePath", "/test/test")
+            .set("X-Auth-Token", "test")
+            .expect(404, done);
+        });
+    });
+    describe("POST /v2/entities", function() {
+        it("should return HTTP 200 when all entities are successfully passed checks", (done) => {
+            request(service)
+            .post("/v2/entities")
+            .field("userFile", fs.readFileSync(__dirname + '/working_test.csv', 'utf-8'))
+            .attach('files', __dirname + '/working_test.csv', 'working_test.csv')
+            .set("Fiware-Service", "test")
+            .set("Fiware-ServicePath", "/test/test")
+            .set("X-Auth-Token", "test")
+            .expect(200, done);
+        });
+    });
+    describe("POST /v2/entities", function() {
+        it("should return HTTP 409 when there is difference betweenn rule and entity headers", (done) => {
+            request(service)
+            .post("/v2/entities")
+            .field("userFile", fs.readFileSync(__dirname + '/headerFail_test.csv', 'utf-8'))
+            .attach('files', __dirname + '/headerFail_test.csv', 'headerFail_test.csv')
+            .set("Fiware-Service", "test")
+            .set("Fiware-ServicePath", "/test/test")
+            .set("X-Auth-Token", "test")
+            .expect(409, done);
+        });
+    });
+    describe("POST /v2/entities", function() {
+        it("should return HTTP 403 when there is no type to find rules by", (done) => {
+            request(service)
+            .post("/v2/entities")
+            .field("userFile", fs.readFileSync(__dirname + '/typeFail_test.csv', 'utf-8'))
+            .attach('files', __dirname + '/typeFail_test.csv', 'typeFail_test.csv')
             .set("Fiware-Service", "test")
             .set("Fiware-ServicePath", "/test/test")
             .set("X-Auth-Token", "test")

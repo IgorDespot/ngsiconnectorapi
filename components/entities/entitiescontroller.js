237225805c7f66961d07197fbe3a51a5ccfc46c7
Igor Despot <igordesspot@gmail.com>
2018-12-20 07:02:29 -0800

    Add entity creation to controller

--- components/entities/entitiescontroller.js
+++ components/entities/entitiescontroller.js
@@ -3,6 +3,10 @@
 var request = require("request-promise");
 var config = require("../../config");
 
+var { 
+    parseRawData
+} = require("./entitiesservice");
+
 function retriveAllEntities(contextObject) {
     return request({
         method: "GET",
@@ -29,7 +33,64 @@ function retriveSingleEntity(contextObject) {
     });
 };
 
+function postEntitiesInBatch(contextObject, data) {
+    return request({
+        method: "POST",
+        headers: {
+            "Fiware-Service": contextObject.fiwareService,
+            "Fiware-ServicePath": contextObject.fiwareServicePath,
+            "X-Auth-Token": contextObject.authToken
+        },
+        uri: `${config.fiware.orion_url}/v2/op/update`,
+        body: {
+            actionType: "APPEND",
+            entities: data
+        },
+        json: true
+    });
+};
+
+async function createEntitiesBatch(contextObject, mode) {
+
+    try {
+
+        var parsedData = await parseRawData(contextObject, mode);
+        
+        var parsedDataForCreation = chunk(parsedData.res, 1);
+            
+        var final = [];
+
+        for (let single of parsedDataForCreation) {
+            final.push(postEntitiesInBatch(contextObject, single));
+        }
+
+        let orionErrorCheck = await Promise.all(final);
+
+        return parsedData;
+
+    } catch (error) {
+        if (error.statusCode) {
+            throw { code: error.statusCode, msg: {
+                error: error.name,
+                info: error.error.description
+            }}
+        }
+        throw error;
+    }
+};
+
+function chunk(array, size) {
+    const chunked_arr = [];
+    let copied = [...array];
+    const numOfChild = Math.ceil(copied.length / size);
+    for (let i = 0; i < numOfChild; i++) {
+        chunked_arr.push(copied.splice(0, size));
+    }
+    return chunked_arr;
+}
+
 module.exports = {
     retriveAllEntities,
-    retriveSingleEntity
+    retriveSingleEntity,
+    createEntitiesBatch
 }
